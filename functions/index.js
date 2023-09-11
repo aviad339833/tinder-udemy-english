7dff50cc3767ac6ecb3edda2fe25ff328532c8e5
aviad339833 <aviad339833@gmail.com>
2023-09-10 22:27:38 -0400

    new stracture

--- functions/index.js
+++ functions/index.js
@@ -1,139 +1,129 @@
 const functions = require("firebase-functions");
-// Import the Admin Library
-const admin = require('firebase-admin');
+const admin = require("firebase-admin");
+const express = require("express");
+const cors = require("cors");
 
-// We initialize the admin library
 admin.initializeApp();
-
-// From the admin library, we invoke the firestore module
 const firestore = admin.firestore();
 
- exports.get = functions.https.onRequest( async (request, response) => {
-  // functions.logger.info("Hello logs!", {structuredData: true});
-  
+const app = express();
+app.use(cors({ origin: true }));
+app.use(express.json());
 
-  // We execute an insertion inside the users collection of the object {name:'jesse'}
-  const result = await firestore.collection('users').add({name:'Jesse'})
+app.get("/", async (req, res) => {
+  try {
+    const result = await firestore.collection("users").add({ name: "Jesse" });
+    res.status(200).send(result);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
 
-  // I return the result to the navigator
-  response.send(result);
- });
+app.post("/", async (req, res) => {
+  try {
+    const { type, myId, idOfPersonThatILike, idOfPersonThatIDontLike } =
+      req.body;
 
+    switch (type) {
+      case "personLikesMe":
+        await firestore
+          .collection("users")
+          .doc(idOfPersonThatILike)
+          .collection("theyLikeMe")
+          .doc(myId)
+          .set(
+            {
+              uid: myId,
+              documentReference: firestore.collection("users").doc(myId),
+            },
+            { merge: true }
+          );
+        res.status(200).send("Successful");
+        break;
 
- exports.post = functions.https.onRequest( async (request, response) => {
-   // functions.logger.info("Hello logs!", {structuredData: true});
+      case "IDontLikeYou":
+        await firestore
+          .collection("users")
+          .doc(myId)
+          .collection("theyLikeMe")
+          .doc(idOfPersonThatIDontLike)
+          .delete();
+        res.status(200).send("Successfully Deleted");
+        break;
 
-   const body = request.body
+      case "weLikeEachOther":
+        const otherPersonObject = {
+          uid: idOfPersonThatILike,
+          documentReference: firestore
+            .collection("users")
+            .doc(idOfPersonThatILike),
+        };
+        const myObject = {
+          uid: myId,
+          documentReference: firestore.collection("users").doc(myId),
+        };
+        await firestore
+          .collection("users")
+          .doc(idOfPersonThatILike)
+          .collection("weLikeEachOther")
+          .doc(myId)
+          .set(myObject, { merge: true });
+        await firestore
+          .collection("users")
+          .doc(myId)
+          .collection("weLikeEachOther")
+          .doc(idOfPersonThatILike)
+          .set(otherPersonObject, { merge: true });
+        await firestore
+          .collection("users")
+          .doc(myId)
+          .collection("theyLikeMe")
+          .doc(idOfPersonThatILike)
+          .delete();
+        const idOfDocument = generateChatId(myId, idOfPersonThatILike);
+        await firestore
+          .collection("chats")
+          .doc(idOfDocument)
+          .set(
+            {
+              idsConcatenated: idOfDocument,
+              arrayOfPeopleInConversation: [myId, idOfPersonThatILike],
+            },
+            { merge: true }
+          );
+        res.status(200).send("We like Each other successfully done");
+        break;
 
-   const type = body.type
-   console.log('body', body)
+      case "breakMatch":
+        const id = generateChatId(myId, idOfPersonThatIDontLike);
+        const listMessageDocuments = await firestore
+          .collection("chats")
+          .doc(id)
+          .collection("messages")
+          .listDocuments();
+        listMessageDocuments.forEach((eachDoc) => {
+          eachDoc.delete();
+        });
+        await firestore.collection("chats").doc(id).delete();
+        const path1 = `users/${myId}/weLikeEachOther/${idOfPersonThatIDontLike}`;
+        const path2 = `users/${idOfPersonThatIDontLike}/weLikeEachOther/${myId}`;
+        await firestore.doc(path1).delete();
+        await firestore.doc(path2).delete();
+        res.status(200).send("Deletion successful");
+        break;
 
-  if( type === 'personLikesMe'){
-    const myId = body.myId
-    const idOfPersonThatILike = body.idOfPersonThatILike
-    await firestore.collection('users').doc(idOfPersonThatILike).collection('theyLikeMe').doc(myId).set(
-      {
-        uid: myId,
-        documentReference: firestore.collection('users').doc(myId)
+      default:
+        res.status(400).send("Invalid type provided");
     }
-    ,{ merge: true})
-    response.send("Successfull")
+  } catch (error) {
+    res.status(500).send(error.message);
   }
+});
 
-  if( type === 'IDontLikeYou'){
-    const myId = body.myId
+exports.api = functions.https.onRequest(app);
 
-    //idOfPersonThatIDontLike
-    const idOfPersonThatIDontLike = body.idOfPersonThatIDontLike
-    console.log('data', myId, idOfPersonThatIDontLike)
-    await firestore.collection('users').doc(myId).collection('theyLikeMe').doc(idOfPersonThatIDontLike).delete()
-    response.send("Successfully Deleted")
-  }
-
-  if (type === 'weLikeEachOther'){
-
-    const myId = body.myId
-    const idOfPersonThatILike = body.idOfPersonThatILike
-
-    // I prepare the 2 objects
-    //  otherPersonObject
-    const otherPersonObject =  {
-      uid: idOfPersonThatILike,
-      documentReference: firestore.collection('users').doc(idOfPersonThatILike)
-    }
-    // My object
-
-    const myObject =  {
-      uid: myId,
-      documentReference: firestore.collection('users').doc(myId)
-    }
-
-    // 2Inserts in weLikeEach other subcollection
-    await firestore.collection('users').doc(idOfPersonThatILike).collection('weLikeEachOther').doc(myId).set( myObject ,{ merge: true})
-    await firestore.collection('users').doc(myId).collection('weLikeEachOther').doc(idOfPersonThatILike).set( otherPersonObject ,{ merge: true})
-
-
-    // Delete the document from my subcollection of "theyLikeMe"
-    await firestore.collection('users').doc(myId).collection('theyLikeMe').doc(idOfPersonThatILike).delete()
-
-    // Create the chat Document in the chat collection
-    const idOfDocument = generateChatId(myId, idOfPersonThatILike)
-
-    await firestore.collection('chats').doc(idOfDocument).set({
-      idsConcatenated: idOfDocument,
-      arrayOfPeopleInConversation: [myId, idOfPersonThatILike]
-    }, {merge:true})
-
-
-
-    response.send("We like Each other successfully done");
-  }
-
-  if( type === 'breakMatch'){
-
-    console.log('break match')
-    // Get the data passed through the API CALL
-    
-    const myId = body.myId
-    const idOfPersonThatIDontLike = body.idOfPersonThatIDontLike
-    console.log('my id', myId)
-    console.log('personIdonTLike', idOfPersonThatIDontLike)
-    // Delete all the Chat
-    const id = generateChatId(myId, idOfPersonThatIDontLike)
-    console.log('id', id)
-    const listMessageDocuments = await firestore.collection('chats').doc(id).collection('messages').listDocuments()
-    console.log('list of message doc', listMessageDocuments)
-    listMessageDocuments.forEach((eadhDoc)=>{
-      eadhDoc.delete()
-    })
-
-    await firestore.collection('chats').doc(id).delete()
-
-
-    // Delete the user from the "weLikeEachOther" subcollections in both places, in mine, and in the other person's subcollection
-
-    const path1 = `users/${myId}/weLikeEachOther/${idOfPersonThatIDontLike}`
-    const path2 = `users/${idOfPersonThatIDontLike}/weLikeEachOther/${myId}`
-
-    // Perform the delete operations
-    await firestore.doc(path1).delete()
-    await firestore.doc(path2).delete()
-
-    response.send('Deletion successfull')
-  }
-
-
-
-
-
-   response.send("Hello i am a POST");
-  });
-
-
-  const generateChatId = (id1, id2) =>{
-    const array = [id1, id2]
-    array.sort()
-    return `${array[0]}-${array[1]}` 
-
-  }
- 
+const generateChatId = (id1, id2) => {
+  const array = [id1, id2];
+  array.sort();
+  return `${array[0]}-${array[1]}`;
+};
