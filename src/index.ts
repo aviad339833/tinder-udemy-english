d5f6c3867c8b5cc607c7ece3a5989ff7079ca50d
aviad339833 <aviad339833@gmail.com>
2023-09-22 11:23:57 -0400

    good state I think

--- src/index.ts
+++ src/index.ts
@@ -5,24 +5,21 @@ initializeApp();
 
 import * as functions from 'firebase-functions';
 import {
+  checkForMatchAndCreateChat,
+  dislikeUser,
   fetchAllMatchesForUser,
-  fetchAllUsers,
+  fetchPotentialMatches,
   fetchUsersILike,
 } from './userHandlers';
-import {
-  handlePersonThatIDontLike,
-  handlePersonThatILike,
-} from './userInteractions';
 import { resetAllUsers } from './reseteDb';
 
-const app = express(); // Create an instance of the Express application
+const app = express();
 
 interface IRequest {
   type: string;
   myId: string;
   ThePersonThatILiked?: string;
   thePersonThatIDontLiked?: string;
-  // Add any other properties as needed
 }
 
 interface IQuery {
@@ -35,62 +32,21 @@ app.get('/', async (request: express.Request, response: express.Response) => {
   const query = request.query as unknown as IQuery;
 
   switch (query.type) {
-    case 'getAllMatchedUsers':
-      try {
-        // Use the fetchAllUsers function without passing lastUserId
-        const users = await fetchAllUsers(query.userId);
-        response.send(users);
-      } catch (error) {
-        console.error('Error fetching users:', error);
-        response.status(500).send('Error fetching users: ' + error);
-      }
-      break;
-
-    case 'fetchAllMatches':
-      if (!query.userId) {
-        const errorMessage =
-          'Required parameter (myId) is missing for fetching matches.';
-        console.error(errorMessage);
-        return response.status(400).send(errorMessage);
-      }
-
-      try {
-        // Call the function to fetch all matches for the user and get its result
-        const matches = await fetchAllMatchesForUser(query.userId);
-
-        // Send back the list of matches
-        response.send(matches);
-      } catch (error) {
-        console.error('Error fetching matches:', error);
-        response.status(500).send(`Error fetching matches: ${error}`);
-      }
-      break;
-
-    case 'fetchUsersILike': {
-      // Added a block here
-      if (!query.userId) {
-        const errorMessage =
-          'Required parameter (userId) is missing for fetching users I like.';
-        console.error(errorMessage);
-        return response.status(400).send(errorMessage);
-      }
-
-      // Check if a limit is provided
-      const limit = 20;
-
-      try {
-        // Call the function to fetch users the given user likes and get its result
-        const likedUsers = await fetchUsersILike(query.userId, limit);
-
-        // Send back the list of liked users
-        response.send({ likedUsers });
-      } catch (error) {
-        console.error('Error fetching users I like:', error);
-        response.status(500).send(`Error fetching users I like: ${error}`);
-      }
+    case 'fetchPotentialMatches': {
+      const users = await fetchPotentialMatches(query.userId);
+      response.send(users);
       break;
     }
-    // ... add more cases as needed
+    case 'fetchAllMatches': {
+      const matches = await fetchAllMatchesForUser(query.userId);
+      response.send(matches);
+      break;
+    }
+    case 'fetchUsersILike': {
+      const likedUsers = await fetchUsersILike(query.userId, query.limit || 20);
+      response.send({ likedUsers });
+      break;
+    }
     default:
       response.send('Unknown GET action');
       break;
@@ -99,57 +55,34 @@ app.get('/', async (request: express.Request, response: express.Response) => {
 
 app.post('/', async (request: express.Request, response: express.Response) => {
   const body: IRequest = request.body;
-  console.log('body.type start', body.type);
-  console.log('body', body);
 
   switch (body.type) {
-    case 'personThatILike':
-      if (!body.myId || !body.ThePersonThatILiked) {
-        const errorMessage = 'Required parameters are missing.';
-        console.error(errorMessage);
-        return response.status(400).send(errorMessage);
+    case 'personThatILike': {
+      if (!body.ThePersonThatILiked) {
+        return response
+          .status(400)
+          .send('ThePersonThatILiked parameter is missing.');
       }
-
-      try {
-        // Call the function to handle the like interaction and get its result
-        const matchResult = await handlePersonThatILike(
-          body.myId,
-          body.ThePersonThatILiked
-        );
-
-        // Send back the result from the handlePersonThatILike function
-        response.send(matchResult);
-      } catch (error) {
-        console.error('Error processing interaction:', error);
-        response.status(500).send(`Error processing interaction: ${error}`);
-      }
+      const matchResult = await checkForMatchAndCreateChat(
+        body.myId,
+        body.ThePersonThatILiked
+      );
+      response.send(matchResult);
       break;
-
-    case 'personThatIDislike':
-      if (!body.myId || !body.thePersonThatIDontLiked) {
-        const errorMessage = 'Required parameters are missing for not liking.';
-        console.error(errorMessage);
-        return response.status(400).send(errorMessage);
+    }
+    case 'personThatIDislike': {
+      if (!body.thePersonThatIDontLiked) {
+        return response
+          .status(400)
+          .send('ThePersonThatILiked parameter is missing.');
       }
-
-      try {
-        // Call the function to handle the interaction for not liking
-        const notLikeResult = await handlePersonThatIDontLike(
-          body.myId,
-          body.thePersonThatIDontLiked
-        );
-
-        // Send back the result from the handlePersonThatIDontLike function
-        response.send(notLikeResult);
-      } catch (error) {
-        console.error('Error processing not liking interaction:', error);
-        response
-          .status(500)
-          .send(`Error processing not liking interaction: ${error}`);
-      }
+      const notLikeResult = await dislikeUser(
+        body.myId,
+        body.thePersonThatIDontLiked
+      );
+      response.send(notLikeResult);
       break;
-
-    // ... add more cases as needed
+    }
     default:
       response.send('Unknown POST action');
       break;
@@ -157,13 +90,8 @@ app.post('/', async (request: express.Request, response: express.Response) => {
 });
 
 app.delete('/reset-all-users', async (request, response) => {
-  try {
-    await resetAllUsers();
-    response.status(200).send('All users have been reset.');
-  } catch (error) {
-    console.error('Error resetting all users:', error);
-    response.status(500).send(`Error resetting all users: ${error}`);
-  }
+  await resetAllUsers();
+  response.status(200).send('All users have been reset.');
 });
 
 export const api = functions.https.onRequest(app);
--- src/userHandlers.ts
+++ src/userHandlers.ts
@@ -1,124 +1,186 @@
 import * as admin from 'firebase-admin';
 
 const firestore = admin.firestore();
-const PAGE_SIZE = 20;
 
-export const fetchAllUsers = async (
-  excludeUserId: string
+export const fetchPotentialMatches = async (
+  userId: string
 ): Promise<admin.firestore.DocumentData[]> => {
-  console.log(`Fetching batch for user with ID: ${excludeUserId}`);
+  console.log(`Fetching potential matches for user with ID: ${userId}`);
 
-  const interactionsCollection = firestore.collection('interactions');
+  const TARGET_SIZE = 20; // Desired number of potential matches
+  const BATCH_SIZE = 100; // Fetch size for each query
 
-  const likedAndDislikedUsers = await interactionsCollection
-    .where('userId', '==', excludeUserId)
-    .get();
-
-  const excludedUserIds = likedAndDislikedUsers.docs.map((doc) =>
-    doc.get('otherUserId')
-  );
-
-  excludedUserIds.push(excludeUserId); // Add the user themselves to the exclude list
-
-  const currentUserDoc = await firestore
+  const interactionsCollection = firestore
     .collection('users')
-    .doc(excludeUserId)
-    .get();
-  const currentUserData = currentUserDoc.data();
+    .doc(userId)
+    .collection('interactions');
+  const likedAndDislikedUsers = await interactionsCollection.get();
+  const excludedUserIds = likedAndDislikedUsers.docs.map((doc) => doc.id);
+  excludedUserIds.push(userId); // Exclude the user themself
 
-  if (!currentUserData) {
-    throw new Error('User not found');
-  }
+  const currentUserDoc = await firestore.collection('users').doc(userId).get();
+  const currentUserData = currentUserDoc.data();
+  if (!currentUserData) throw new Error('User not found');
 
   const userInterestInGender = currentUserData.userInterestInGender;
-  const batchSize = 20;
 
-  const userQuery = firestore
-    .collection('users')
-    .where('gender', '==', userInterestInGender)
-    .orderBy('created_time')
-    .limit(batchSize);
+  let lastDoc; // To keep track of where to start in the next batch
+  let potentialMatches: admin.firestore.DocumentData[] = [];
 
-  const usersSnapshot = await userQuery.get();
+  while (potentialMatches.length < TARGET_SIZE) {
+    let usersQuery = firestore
+      .collection('users')
+      .where('gender', '==', userInterestInGender)
+      .orderBy('created_time')
+      .limit(BATCH_SIZE);
 
-  const users: admin.firestore.DocumentData[] = [];
-  usersSnapshot.forEach((doc) => {
-    if (!excludedUserIds.includes(doc.id)) {
-      users.push({ id: doc.id, ...doc.data() });
+    if (lastDoc) {
+      usersQuery = usersQuery.startAfter(lastDoc);
     }
-  });
+
+    const usersSnapshot = await usersQuery.get();
+    if (usersSnapshot.empty) break; // Exit if no more users to fetch
+
+    lastDoc = usersSnapshot.docs[usersSnapshot.docs.length - 1];
+
+    const filteredUsers = usersSnapshot.docs
+      .filter((doc) => !excludedUserIds.includes(doc.id))
+      .map((doc) => ({ id: doc.id, ...doc.data() }));
+
+    potentialMatches.push(...filteredUsers);
+  }
+
+  // If there are more than the target size due to the last batch, slice it
+  if (potentialMatches.length > TARGET_SIZE) {
+    potentialMatches = potentialMatches.slice(0, TARGET_SIZE);
+  }
 
   console.log(
-    `Fetched ${users.length} users excluding user with ID: ${excludeUserId} and other excluded users.`
+    `Fetched ${potentialMatches.length} potential matches for user with ID: ${userId}.`
   );
-
-  return users;
+  return potentialMatches;
 };
 
-export const fetchAllMatchesForUser = async (
-  userId: string,
-  lastUserTimestamp?: FirebaseFirestore.Timestamp
-): Promise<{
-  matches: string[];
-  lastTimestamp?: FirebaseFirestore.Timestamp;
-}> => {
+export const dislikeUser = async (
+  currentUserId: string,
+  targetUserId: string
+) => {
   console.log(
-    `[fetchAllMatchesForUser] Fetching matches for user with ID: ${userId}`
+    `User with ID ${currentUserId} dislikes user with ID ${targetUserId}`
   );
 
-  const interactionsCollection = firestore.collection('interactions');
-  const likedUsers: string[] = [];
-  const matches: string[] = [];
+  // 1. Recording the Dislike
+  const currentUserInteractions = firestore
+    .collection('users')
+    .doc(currentUserId)
+    .collection('interactions');
 
-  const myRef = firestore.collection('users').doc(userId); // Reference to the current user's document
+  const dislikeData = {
+    targetUserId: targetUserId,
+    interactionType: 'dislike',
+    timestamp: admin.firestore.FieldValue.serverTimestamp(),
+  };
 
-  try {
-    let query = interactionsCollection
-      .where('userRef', '==', myRef)
-      .where('interactionType', '==', 'like')
-      .orderBy('timestamp', 'desc')
-      .limit(PAGE_SIZE);
+  // eslint-disable-next-line max-len
+  await currentUserInteractions.doc(targetUserId).set(dislikeData); // Here the document ID is set as the targetUserId to prevent duplication
 
-    if (lastUserTimestamp) {
-      query = query.startAfter(lastUserTimestamp);
-    }
+  // 2. Optionally Notify the Disliked User
+  // eslint-disable-next-line max-len
+  // (This step can be a push notification or an update in some sort of "views" count, without specifying it was a dislike)
 
-    const likesSnapshot = await query.get();
+  // 3. Update Metrics (if applicable)
+  // (This can involve updating some metrics in the users' collection or another analytics system)
 
-    if (likesSnapshot.empty) {
-      return { matches: [] };
-    }
+  console.log(
+    `Dislike recorded for user with ID ${targetUserId} by user with ID ${currentUserId}`
+  );
+};
 
-    likesSnapshot.forEach((doc) => {
-      const otherUserRef = doc.data().otherUserRef;
-      likedUsers.push(otherUserRef.id);
-    });
+export const likeUser = async (currentUserId: string, targetUserId: string) => {
+  console.log(
+    `User with ID ${currentUserId} likes user with ID ${targetUserId}`
+  );
 
-    for (const otherUserId of likedUsers) {
-      const otherUserRef = firestore.collection('users').doc(otherUserId);
+  // Record the Like for the current user
+  const currentUserInteractions = firestore
+    .collection('users')
+    .doc(currentUserId)
+    .collection('interactions');
 
-      const mutualLikeSnapshot = await interactionsCollection
-        .where('userRef', '==', otherUserRef)
-        .where('otherUserRef', '==', myRef)
-        .where('interactionType', '==', 'like')
-        .limit(1)
-        .get();
+  const likeData = {
+    targetUserId: targetUserId,
+    interactionType: 'like',
+    timestamp: admin.firestore.FieldValue.serverTimestamp(),
+  };
 
-      if (!mutualLikeSnapshot.empty) {
-        matches.push(`${otherUserId}`);
-      }
-    }
+  await currentUserInteractions.doc(targetUserId).set(likeData);
 
-    const lastTimestamp =
-      likesSnapshot.docs[likesSnapshot.docs.length - 1].data().timestamp;
+  console.log(
+    `Like recorded for user with ID ${targetUserId} by user with ID ${currentUserId}`
+  );
+};
 
-    return { matches, lastTimestamp };
-  } catch (error) {
-    console.error(
-      `[fetchAllMatchesForUser] Error fetching matches for user ${userId}: ${error}`
+export const checkForMatchAndCreateChat = async (
+  currentUserId: string,
+  targetUserId: string
+) => {
+  console.log(
+    `Checking for match between ${currentUserId} and ${targetUserId}`
+  );
+
+  const targetUserInteractions = firestore
+    .collection('users')
+    .doc(targetUserId)
+    .collection('interactions');
+
+  const interactionDoc = await targetUserInteractions.doc(currentUserId).get();
+
+  if (
+    interactionDoc.exists &&
+    interactionDoc.get('interactionType') === 'like'
+  ) {
+    // eslint-disable-next-line quotes
+    console.log("It's a match!");
+
+    // Record the match for both users
+    const currentUserMatches = firestore
+      .collection('users')
+      .doc(currentUserId)
+      .collection('matches');
+    const targetUserMatches = firestore
+      .collection('users')
+      .doc(targetUserId)
+      .collection('matches');
+
+    const matchData = {
+      matchedUserId: targetUserId,
+      timestamp: admin.firestore.FieldValue.serverTimestamp(),
+    };
+
+    await currentUserMatches.doc(targetUserId).set(matchData);
+
+    matchData.matchedUserId = currentUserId;
+    await targetUserMatches.doc(currentUserId).set(matchData);
+
+    // Create a new chat for the matched users
+    const chatData = {
+      user1Id: currentUserId,
+      user2Id: targetUserId,
+      lastMessageTimestamp: admin.firestore.FieldValue.serverTimestamp(),
+    };
+
+    const chatRef = await firestore.collection('chats').add(chatData);
+
+    // Optionally notify both users about the match
+
+    console.log(
+      `Matched and chat created for ${currentUserId} and ${targetUserId}`
     );
-    throw new Error(`Error fetching matches for user: ${error}`);
+    return chatRef.id; // return the chat ID if you want to redirect users to the chat immediately
   }
+
+  console.log(`No match found between ${currentUserId} and ${targetUserId}`);
+  return null;
 };
 
 export const fetchUsersILike = async (
@@ -159,5 +221,71 @@ export const fetchUsersILike = async (
       `[fetchUsersILike] Error fetching liked users for user ${userId}: ${error}`
     );
     throw new Error(`Error fetching liked users for user: ${error}`);
+  }
+};
+
+export const fetchAllMatchesForUser = async (
+  userId: string,
+  lastUserTimestamp?: FirebaseFirestore.Timestamp
+): Promise<{
+  matches: string[];
+  lastTimestamp?: FirebaseFirestore.Timestamp;
+}> => {
+  console.log(
+    `[fetchAllMatchesForUser] Fetching matches for user with ID: ${userId}`
+  );
+
+  const interactionsCollection = firestore.collection('interactions');
+  const likedUsers: string[] = [];
+  const matches: string[] = [];
+
+  const myRef = firestore.collection('users').doc(userId); // Reference to the current user's document
+
+  try {
+    let query = interactionsCollection
+      .where('userRef', '==', myRef)
+      .where('interactionType', '==', 'like')
+      .orderBy('timestamp', 'desc')
+      .limit(10);
+
+    if (lastUserTimestamp) {
+      query = query.startAfter(lastUserTimestamp);
+    }
+
+    const likesSnapshot = await query.get();
+
+    if (likesSnapshot.empty) {
+      return { matches: [] };
+    }
+
+    likesSnapshot.forEach((doc) => {
+      const otherUserRef = doc.data().otherUserRef;
+      likedUsers.push(otherUserRef.id);
+    });
+
+    for (const otherUserId of likedUsers) {
+      const otherUserRef = firestore.collection('users').doc(otherUserId);
+
+      const mutualLikeSnapshot = await interactionsCollection
+        .where('userRef', '==', otherUserRef)
+        .where('otherUserRef', '==', myRef)
+        .where('interactionType', '==', 'like')
+        .limit(1)
+        .get();
+
+      if (!mutualLikeSnapshot.empty) {
+        matches.push(`${otherUserId}`);
+      }
+    }
+
+    const lastTimestamp =
+      likesSnapshot.docs[likesSnapshot.docs.length - 1].data().timestamp;
+
+    return { matches, lastTimestamp };
+  } catch (error) {
+    console.error(
+      `[fetchAllMatchesForUser] Error fetching matches for user ${userId}: ${error}`
+    );
+    throw new Error(`Error fetching matches for user: ${error}`);
   }
 };
